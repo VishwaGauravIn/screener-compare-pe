5b0af9395139e409393b94c52d02090eb89ffd6b
Vishwa Gaurav <vishwagauravin@gmail.com>
2025-10-20 19:03:06 +0530

    using geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
+import './globals.css'
+
+const _geist = Geist({ subsets: ["latin"] });
+const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Compare PE of Companies - ProScreener",
-  description:
-    "use this to compare PE ratios of different companies over time. a tool by Vishwa Gaurav, part of ProScreener.",
-  generator: "@VishwaGauravIn",
-};
+  title: 'Compare PE of Companies - ProScreener',
+  description: 'use this to compare PE ratios of different companies over time. a tool by Vishwa Gaurav, part of ProScreener.',
+  generator: '@VishwaGauravIn',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -21,5 +24,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  );
+  )
 }
